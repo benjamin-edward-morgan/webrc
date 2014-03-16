d7560080fc8533b5de715d1e4a686159d7996d8a
benjamin-edward-morgan <benjamin.morgan@inbloom.org>
2014-03-16 14:19:12 -0400

    i have no idea. did this stuff last week

---
+++ server/src/main/java/webrc/server/ServerMain.java
@@ -0,0 +1,11 @@
+package webrc.server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: benjaminmorgan
+ * Date: 3/9/14
+ * Time: 12:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerMain {
+}
