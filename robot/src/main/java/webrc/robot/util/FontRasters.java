40db9e1d57bc63d3522cb007cb7f05b5aa6eb3ee
Ben Morgan <benjamin.morgan@inBloom.org>
2014-04-10 23:09:58 -0400

    screen memory, text rasterization !

---
+++ robot/src/main/java/webrc/robot/util/FontRasters.java
@@ -0,0 +1,25 @@
+package webrc.robot.util;
+
+import java.awt.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: benjaminmorgan
+ * Date: 4/8/14
+ * Time: 5:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FontRasters {
+
+
+    public static void main(String[] args)
+    {
+
+        Font f = new Font.getFont("Helvetica");
+
+        System.out.println(f);
+
+    }
+
+
+}
