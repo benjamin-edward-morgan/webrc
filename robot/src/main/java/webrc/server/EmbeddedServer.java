2adf82277defbb7d9e4ccbd63d897ecfe3033395
Ben Morgan <benjamin.morgan@inBloom.org>
2014-05-28 01:10:33 -0400

    cometd sorta working

--- robot/src/main/java/webrc/server/EmbeddedServer.java
+++ robot/src/main/java/webrc/server/EmbeddedServer.java
@@ -1,17 +1,21 @@
 package webrc.server;
 
-import org.cometd.bayeux.server.BayeuxServer;
-import org.cometd.bayeux.server.ConfigurableServerChannel;
-import org.cometd.bayeux.server.ServerChannel;
+import org.cometd.bayeux.server.*;
 import org.cometd.server.CometdServlet;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import sun.misc.BASE64Encoder;
+import org.eclipse.jetty.util.resource.Resource;
 
 import javax.annotation.PostConstruct;
+import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created with IntelliJ IDEA.
@@ -29,49 +33,134 @@ public class EmbeddedServer {
     @PostConstruct
     public void init() throws Exception {
 
-// Configure Jetty
         Server server = new Server(8080);
+
+        //static resource handler
+        ResourceHandler resource_handler = new ResourceHandler() {
+            @Override
+            public Resource getResource(String path)
+                    throws MalformedURLException {
+                Resource resource = Resource.newClassPathResource(path);
+                if (resource == null || !resource.exists()) {
+                    resource = Resource.newClassPathResource("META-INF/resources" + path);
+                }
+                return resource;
+            }
+        };
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setWelcomeFiles(new String[] { "static/rc.html" });
+        resource_handler.setResourceBase("static");
+        resource_handler.setDirectoriesListed(true);
+
+        //cometd context
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
+        context.setContextPath("/webrc");
 
-// Configure Cometd
-        CometdServlet servlet = new CometdServlet();
-        context.addServlet(new ServletHolder(servlet), "/bayeux");
-// Start Jetty
-        //server.start(context);
+        // Configure Cometd
+//        CometdServlet servlet = new CometdServlet();
+//
+//        System.out.println(servlet.getServletConfig());
+//        System.out.println(servlet.getServletInfo());
+//
+//        context.addServlet(new ServletHolder(servlet), "/bayeux");
+//
+        ServletHolder holder = context.addServlet(CometdServlet.class, "/bayeux");
+        context.addServlet(CometdServlet.class, "/bayeux/handshake");
+        context.addServlet(CometdServlet.class, "/bayeux/connect");
+        context.addServlet(CometdServlet.class, "/bayeux/");
 
 
-// Create a channel for our Java server to publish on
+
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] {resource_handler, context});
+        server.setHandler(handlers);
+
+
+        server.start();
+
+        CometdServlet servlet = (CometdServlet) holder.getServlet();
+
         BayeuxServer bayeux = servlet.getBayeux();
+
         String channelName = "/data";
-        bayeux.createIfAbsent(channelName, new ServerChannel.Initializer()
-        {
-            public void configureChannel(ConfigurableServerChannel channel)
-            {
-                channel.setPersistent(true);
+        bayeux.createChannelIfAbsent(channelName, new ServerChannel.Initializer() {
+            @Override
+            public void configureChannel(ConfigurableServerChannel configurableServerChannel) {
+                configurableServerChannel.setPersistent(true);
             }
         });
         final ServerChannel channel = bayeux.getChannel(channelName);
 
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run() {
+
+                Map<String, Double> data = new HashMap<String,Double>();
+                data.put("random", Math.random() * 100);
+                channel.publish(null, data);
+
+            }
+        }, 0, 500);
 
 
-// publish some random data
-        byte [] bytes = new byte[40];
-        for (int i = 0; i < bytes.length; ++i) {
-            bytes[i] = (byte)(Math.random() * 255);
-        }
+        channel.addListener(new ServerChannel.MessageListener(){
+            @Override
+            public boolean onMessage(ServerSession serverSession, ServerChannel serverChannel, ServerMessage.Mutable mutable) {
 
-        BASE64Encoder base64Encoder = new sun.misc.BASE64Encoder();
-        final String data = base64Encoder.encode(bytes);
+                System.out.println("Received: " + mutable.getData());
 
-        final Map<String, String> firstData = new HashMap<>();
-        firstData.put("a", "b");
-        firstData.put("something", "val");
+                return false;
 
-// Maps are converted to JavaScript objects
-        final Map<String, String> secondData = new HashMap<>();
-        secondData.put("base64data", data);
+            }
+        });
+
+        server.join();
+
+// Configure Jetty
+//        Server server = new Server(8080);
+//        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+//        context.setContextPath("/");
+//        server.setHandler(context);
+//
+//// Configure Cometd
+//        CometdServlet servlet = new CometdServlet();
+//        context.addServlet(new ServletHolder(servlet), "/bayeux");
+//// Start Jetty
+//        server.start();
+
+
+// Create a channel for our Java server to publish on
+//        BayeuxServer bayeux = servlet.getBayeux();
+//        String channelName = "/data";
+//        bayeux.createIfAbsent(channelName, new ServerChannel.Initializer()
+//        {
+//            public void configureChannel(ConfigurableServerChannel channel)
+//            {
+//                channel.setPersistent(true);
+//            }
+//        });
+//        final ServerChannel channel = bayeux.getChannel(channelName);
+//
+//
+//
+//// publish some random data
+//        byte [] bytes = new byte[40];
+//        for (int i = 0; i < bytes.length; ++i) {
+//            bytes[i] = (byte)(Math.random() * 255);
+//        }
+//
+//        BASE64Encoder base64Encoder = new sun.misc.BASE64Encoder();
+//        final String data = base64Encoder.encode(bytes);
+//
+//        final Map<String, String> firstData = new HashMap<>();
+//        firstData.put("a", "b");
+//        firstData.put("something", "val");
+//
+//// Maps are converted to JavaScript objects
+//        final Map<String, String> secondData = new HashMap<>();
+//        secondData.put("base64data", data);
 
 
 //        exec.scheduleAtFixedRate(new Runnable() {
@@ -86,6 +175,6 @@ public class EmbeddedServer {
 //        }, 1, 1, TimeUnit.SECONDS);
 
 // Keep running until the server dies
-        server.join();
+      //  server.join();
     }
 }
